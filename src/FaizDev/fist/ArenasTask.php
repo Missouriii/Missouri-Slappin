90b03d90d4503476ab5e635ae9b493bb7b3f89c9
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-04-08 10:50:50 +0300

    PM4

---
+++ src/FaizDev/fist/ArenasTask.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace FaizDev\fist;
+
+use pocketmine\scheduler\Task;
+
+class ArenasTask extends Task {
+	
+	/** @var Main */
+	private $plugin;
+	
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	public function onRun(): void{
+		foreach ($this->plugin->getArenas() as $arena){
+			$arena->tick();
+		}
+	}
+}
